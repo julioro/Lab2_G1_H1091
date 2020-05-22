50e905f6934e6c0b415906e9d274364dc7a26c54
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-22 17:19:56 -0500

    AppBar principal con los 2 botones agregado

--- app/src/main/java/com/example/lab2_g1_h1091/MainActivity.java
+++ app/src/main/java/com/example/lab2_g1_h1091/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.lab2_g1_h1091;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
