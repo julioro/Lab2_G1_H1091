b7feb7f695d14b9a93045b550b80eb211c8a45dd
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-23 16:23:17 -0500

    RV de empleadosActivity

--- app/src/main/java/com/example/lab2_g1_h1091/utilitary/ListaEmpleadosAdapter.java
+++ app/src/main/java/com/example/lab2_g1_h1091/utilitary/ListaEmpleadosAdapter.java
@@ -15,8 +15,8 @@ import com.example.lab2_g1_h1091.entidades.Empleado;
 
 public class ListaEmpleadosAdapter extends RecyclerView.Adapter<ListaEmpleadosAdapter.EmpleadoViewHolder> {
 
-    Empleado[] data;
-    Context contexto;
+    private Empleado[] data;
+    private Context contexto;
 
     public ListaEmpleadosAdapter(Empleado[] lista, Context c) {
         this.data = lista;
@@ -27,14 +27,14 @@ public class ListaEmpleadosAdapter extends RecyclerView.Adapter<ListaEmpleadosAd
     public static class EmpleadoViewHolder extends RecyclerView.ViewHolder {
 
         TextView textView;
-        Button button1;
-        Button button2;
+        //Button button1;
+        //Button button2;
 
         public EmpleadoViewHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.textView1);
-            button1 = itemView.findViewById(R.id.button1);
-            button2 = itemView.findViewById(R.id.button2);
+            //button1 = itemView.findViewById(R.id.button1);
+            //button2 = itemView.findViewById(R.id.button2);
         }
     }
 
