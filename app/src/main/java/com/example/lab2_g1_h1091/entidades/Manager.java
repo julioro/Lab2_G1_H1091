3ea0309cad4ed7501bfda1bc8fab97c64ce53124
JPVEGA <thekindofblizzard@gmail.com>
2020-05-22 20:17:21 -0500

    getters and setters

--- app/src/main/java/com/example/lab2_g1_h1091/entidades/Manager.java
+++ app/src/main/java/com/example/lab2_g1_h1091/entidades/Manager.java
@@ -1,4 +1,41 @@
 package com.example.lab2_g1_h1091.entidades;
 
 public class Manager {
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
