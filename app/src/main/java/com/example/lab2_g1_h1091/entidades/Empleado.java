d34a8219eec8d85e072d9b74c7ee9fbd1abf4d66
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-23 13:55:33 -0500

    Clase DtoEmpleado creada

--- app/src/main/java/com/example/lab2_g1_h1091/entidades/Empleado.java
+++ app/src/main/java/com/example/lab2_g1_h1091/entidades/Empleado.java
@@ -25,5 +25,105 @@ public class Empleado {
     @Size(max=7)
     private String createdBy;
 
+    public Empleado(String employeeId, String firstName, String lastName, String email, String phoneNumber, Trabajo trabajo, Manager manager, double salary, double commissionPct, Department department, String createdBy) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.trabajo = trabajo;
+        this.manager = manager;
+        this.salary = salary;
+        this.commissionPct = commissionPct;
+        this.department = department;
+        this.createdBy = createdBy;
+    }
 
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Trabajo getTrabajo() {
+        return trabajo;
+    }
+
+    public void setTrabajo(Trabajo trabajo) {
+        this.trabajo = trabajo;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getCommissionPct() {
+        return commissionPct;
+    }
+
+    public void setCommissionPct(double commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 }
