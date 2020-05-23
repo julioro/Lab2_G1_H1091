7e3863c6a305fafc68574574577c7f00d074fa52
julioro <julio.rodriguezo@pucp.pe>
2020-05-23 11:07:27 -0500

    Sin conflicto

--- app/src/main/java/com/example/lab2_g1_h1091/entidades/Empleado.java
+++ app/src/main/java/com/example/lab2_g1_h1091/entidades/Empleado.java
@@ -1,12 +1,11 @@
 package com.example.lab2_g1_h1091.entidades;
 
-<<<<<<< .merge_file_a04824
+
 import androidx.annotation.Size;
-=======
+
 import androidx.annotation.FloatRange;
 import androidx.annotation.Size;
 
->>>>>>> .merge_file_a06940
 public class Empleado {
     @Size(max=6)
     private String employeeId;
@@ -18,66 +17,13 @@ public class Empleado {
     private String email;
     @Size(max=20)
     private String phoneNumber;
-    //
     private Trabajo trabajo;
-
-
-    //
     private Manager manager;
-    //
-
     private double salary;
     private double commissionPct;
-
-    //
     private Department department;
-    //
     @Size(max=7)
     private String createdBy;
-
-
-    //getters and SETTER
-
-    public Trabajo getTrabajo() {
-        return trabajo;
-    }
-
-    public void setTrabajo(Trabajo trabajo) {
-        this.trabajo = trabajo;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public double getCommissionPct() {
-        return commissionPct;
-    }
-
-    public void setCommissionPct(double commissionPct) {
-        this.commissionPct = commissionPct;
-    }
-
-    public Manager getManager() {
-        return manager;
-    }
-
-    public void setManager(Manager manager) {
-        this.manager = manager;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
 
 
 }
--- app/src/main/java/com/example/lab2_g1_h1091/entidades/Trabajo.java
+++ app/src/main/java/com/example/lab2_g1_h1091/entidades/Trabajo.java
@@ -4,12 +4,6 @@ import androidx.annotation.Size;
 
 public class Trabajo {
 
-<<<<<<< .merge_file_a12744
-    private String jobId;
-    private String jobTitle;
-    private double minSalary;
-    private double maxSalary;
-    private String createdBy;
 
     public String getJobId() {
         return jobId;
@@ -19,9 +13,6 @@ public class Trabajo {
         this.jobId = jobId;
     }
 
-    public String getJobTitle() {
-        return jobTitle;
-=======
     @Size(max = 10)
     private String jobId;
     @Size(max = 35)
@@ -29,83 +20,8 @@ public class Trabajo {
     private int minSalary;
     private int maxSalary;
     @Size(max = 7, min = 7)
-    private String createdBy = null;
+    private String createdBy;
 
-    public Trabajo(){
 
-    }
-    public Trabajo(String jobId, String jobTitle,int minSalary, int maxSalary, String createdBy){
-        this.jobId = jobId;
-        this.jobTitle = jobTitle;
-        this.minSalary = minSalary;
-        this.maxSalary = maxSalary;
-        this.createdBy = createdBy;
 
-    }
-
-    public void setJobId(String jobId) {
-        this.jobId = jobId;
->>>>>>> .merge_file_a13040
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-<<<<<<< .merge_file_a12744
-    public double getMinSalary() {
-        return minSalary;
-    }
-
-    public void setMinSalary(double minSalary) {
-        this.minSalary = minSalary;
-    }
-
-    public double getMaxSalary() {
-        return maxSalary;
-    }
-
-    public void setMaxSalary(double maxSalary) {
-        this.maxSalary = maxSalary;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-=======
-    public void setMinSalary(int minSalary) {
-        this.minSalary = minSalary;
-    }
-
-    public void setMaxSalary(int maxSalary) {
-        this.maxSalary = maxSalary;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public String getJobId() {
-        return jobId;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public int getMinSalary() {
-        return minSalary;
-    }
-
-    public int getMaxSalary() {
-        return maxSalary;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
->>>>>>> .merge_file_a13040
-    }
 }
