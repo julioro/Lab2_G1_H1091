89bde87a3c886c0862ceb67dd4ae93cf0608304b
JPVEGA <thekindofblizzard@gmail.com>
2020-05-23 13:18:40 -0500

    agregar formulario trabajo

--- app/src/main/java/com/example/lab2_g1_h1091/webservices/WebServices.java
+++ app/src/main/java/com/example/lab2_g1_h1091/webservices/WebServices.java
@@ -85,6 +85,7 @@ public class WebServices {
                     }
                 }) {
         };
+
         return stringRequest;
     }
 }
---
+++ app/src/main/java/com/example/lab2_g1_h1091/webservices/webservice4.java
@@ -0,0 +1,6 @@
+package com.example.lab2_g1_h1091.webservices;
+
+public class webservice4 {
+
+
+}
