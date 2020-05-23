929fc13cccd4cc6f05743d07362455d066febb93
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-23 13:44:11 -0500

    Evento de menu: se puede ir del Main a ListaEmpleados

--- app/src/main/java/com/example/lab2_g1_h1091/MainActivity.java
+++ app/src/main/java/com/example/lab2_g1_h1091/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.lab2_g1_h1091;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.lab2_g1_h1091.entidades.Trabajo;
 import com.example.lab2_g1_h1091.utilitary.ListaTrabajosAdapter;
@@ -18,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuItem1:
+                Intent intent = new Intent(this, EmpleadosActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menuItem2:
+
+                return true;
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
