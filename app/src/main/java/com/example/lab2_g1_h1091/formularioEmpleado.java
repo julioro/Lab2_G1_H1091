fe9b7ea4bf2c1ec33619b201ad6115cff19e0e0c
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-23 21:57:09 -0500

    formulario - nuevo empleado (xml)

---
+++ app/src/main/java/com/example/lab2_g1_h1091/formularioEmpleado.java
@@ -0,0 +1,14 @@
+package com.example.lab2_g1_h1091;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formularioEmpleado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_empleado);
+    }
+}
